3d04c1cc871ac0fc1df62ed59ad353afa6dccab2
Panayiotis Charalambous <panayiotis@cyprus.com>
2025-09-30 16:49:52 +0200

    draw starting background

--- draw.h
+++ draw.h
@@ -3,4 +3,5 @@ void draw_all_pipes(volatile unsigned char *buf);
 void draw_bird(volatile unsigned char *buf);
 void draw_bg(volatile unsigned char *buf);
 void draw_gameover(volatile unsigned char *buf);
+void draw_starting_screen(volatile unsigned char *buf);
 
